1c4f7e75f1fe98841bcbb54741a49aae1ec70bf6
ruofeng <weiruofeng@ruofengx.cn>
2023-07-26 22:23:20 +0800

    添加实验性Tick

--- service/jormungandr/v2/runner/meta.go
+++ service/jormungandr/v2/runner/meta.go
@@ -2,6 +2,7 @@ package runner
 
 import (
 	"jormungandr/v2/proto/base"
+	"reflect"
 	"sync"
 )
 
@@ -22,6 +23,20 @@ func AsyncTick(s *base.Space, f func(*base.Entity)) {
 // 对s中的所有实体有序调用f方法
 func SyncTick(s *base.Space, f func(*base.Entity)) {
 	for _, ent := range s.Entity {
+		f(ent)
+	}
+}
+
+// 对f的name字段进行可选的调用，如果name字段不存在，则不调用f方法
+// 实验性，暂不使用
+func OptionalTick(name string, f func(*base.Entity)) func(*base.Entity) {
+	return func(ent *base.Entity) {
+		// 判断数值是否存在
+		v := reflect.ValueOf(ent)
+		if v.FieldByName(name).IsNil() {
+			return
+		}
+		// 正式调用
 		f(ent)
 	}
 }
